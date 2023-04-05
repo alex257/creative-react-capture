13fbee9e8846bde72fcf78f48b0e7ac53a586034
alex257 <alexalexa802@gmail.com>
2023-04-05 20:17:57 +0200

    add Nav and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { useState } from "react";
+//Global Style
+import GlobalStyle from "./components/GlobalStyle";
 //Import Pages
 import AboutUs from "./pages/AboutUs";
+import Nav from "./components/Nav";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
